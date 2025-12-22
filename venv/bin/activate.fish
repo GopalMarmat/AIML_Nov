ea352695b2cc51c76366b29c0d47eaf4589c6938
GopalMarmat <gopalmarmat256@gmail.com>
2025-12-22 17:42:09 +0530

    Linear and Logistic Regression

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/hp/Documents/AIML_Nov_Batch/venv
+set -gx VIRTUAL_ENV /home/hp/Documents/AIML/AIML_Nov_Batch/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
