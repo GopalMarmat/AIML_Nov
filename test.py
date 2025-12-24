96afa4c21efe53a882b58270476452a157d42d4c
GopalMarmat <gopalmarmat256@gmail.com>
2025-12-24 15:44:20 +0530

    Sample Project

---
+++ test.py
@@ -0,0 +1,7 @@
+def add_number(a,b):
+    return a+b
+
+def sub_num():
+    pass
+def mul_num():
+    pass
---
+++ test1.py
@@ -0,0 +1,6 @@
+from test import add_number
+
+print(add_number(25,29))
+print("Below result from test.py file:")
+
+print(add_number(2506,2910))
